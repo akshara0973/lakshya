76558f7283e3bc0a067cbee0e737ec643a248c7b
AKSHARA SHARMA <aksharasharma916@gmail.com>
2025-05-01 20:37:58 +0530

    ui fix

--- src/Components/Pages/Activities.jsx
+++ src/Components/Pages/Activities.jsx
@@ -6,36 +6,37 @@ export default function Team() {
   const pages = [
     {
       title: "What Lakshya Offers?",
-      content: "Provides vocational training and life skills to help individuals lead independent and fulfilling lives. It focuses on building confidence, self-reliance, and practical skills essential for personal and professional growth. With an emphasis on skilled living.",
+      content: "Offers vocational training and life skills to build confidence, self-reliance, and practical skills for personal and professional growth.",
     },
     {
       title: "Handloom Projects",
 
-      content: "Celebrating the rich tradition of handcrafted textiles while creating sustainable livelihoods for artisans. By blending heritage with innovation and promote eco-friendly practices, weaving a brighter future for all.",
+      content: "Celebrates handcrafted textiles, creating sustainable livelihoods while blending heritage with innovation and eco-friendly practices.",
     },
     {
       title: "Computer Training",
-      content: "It equips individuals with essential digital skills, enabling them to thrive in today’s tech-driven world. It opens doors to learning, and career opportunities, fostering confidence and competence in the digital era.",
+      content: "Equips individuals with digital skills to thrive in a tech-driven world, fostering confidence, learning, and career opportunities.",
     },
     {
       title: "Marketting",
-      content:"Through stalls offers a direct and engaging way to connect with customers. It creates a vibrant platform to showcase products, build brand awareness, and foster meaningful interactions, turning curiosity into lasting relationships.",
+      content:"Stalls provide a vibrant platform to showcase products, connect with customers, and build lasting relationships.",
     },
     {
       title:"NIOS",
-      content:"Schooling through NIOS provides a flexible and inclusive education system, catering to diverse learning needs. It empowers students to pursue education at their own pace, offering academic success and personal growth beyond conventional classrooms.",
+      content:
+      "NIOS schooling offers flexible, education, empowering students to learn at their own pace and achieve academic and personal growth.",
     },
     {
       title:"Sports",
-      content:"Sports inspire physical fitness, mental resilience, and teamwork. They foster discipline, determination, and a sense of community, shaping individuals into confident and well-rounded personalities while promoting a healthy and active lifestyle.",
+      content:"Sports promote physical fitness, mental resilience, shaping confident individuals and encouraging a healthy, active lifestyle.",
     },
     {
       title:"Adventure",
-      content:"It ignites the spirit of curiosity and courage, offering unforgettable experiences and personal growth. It pushes boundaries, and creates lasting memories, inspiring individuals to explore, discover, and live life to the fullest.",
+      content:"It sparks curiosity and courage, offering unforgettable experiences inspiring individuals to explore and live life to the fullest.",
     },
     {
       title:"Crafting Pickles & Spices",
-      content:"Lakshya empowers individuals with disabilities by providing livelihood opportunities. Students even showcase their creativity by making products like pickles and masalas, fostering independence and confidence.",
+      content:"Empowers individuals offering livelihood opportunities & fostering independence through creative products like pickles and masalas.",
     },
     {
       title:"And lot more..."
